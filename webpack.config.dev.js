a7820ca8755de3749c95c89b4a40ad61da9d6232
Dominik H <dcdominoes@gmail.com>
2023-04-10 03:00:21 +0100

    Make root full height to avoid white cutoff on small pages

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,7 +5,6 @@ const base = require('./webpack.config.base');
 module.exports = merge(base, {
   mode: 'development',
   stats: 'none',
-  watch: true,
   devtool: 'eval',
   devServer: {
     static: path.join(__dirname, 'build'),
